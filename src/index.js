0308e94984a8453ad6594d50ae25df08218c9fdd
Kémy CASSIN <kemy.cassin@gmail.com>
2017-07-26 14:25:03 +0200

    add custom button

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import './pdf-reader.css';
 import './components/plugin/TextLayerBuilder.css'
 import PDFReader from './components/PDFReader';
 
-let ToggleBtn = {
-    label: "toggle",
-    classname: "btn"
-};
+let ToggleBtn = <button>toggle panel</button>;
 
 ReactDOM.render( <PDFReader 
     file="/sample.pdf" scale={1}  renderType="svg" btnToggle={ToggleBtn}/> ,
